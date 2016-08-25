4a301af1158f4a3e0fdb57c0f0e78da412c1325e
rickhagenaars <rick@globoweb.nl>
2016-08-25 14:26:20 +0200

    Switched to controlled measurements, added endDate

--- export.py
+++ export.py
@@ -7,9 +7,10 @@ API_SECRET 			= 'sZTgiwMeeAtGRnvtmHGKAEYYs+Rr/uBh1djYAxiUwoM'
 # Generate base64 encoded authorization string
 basicAuthString 	= base64.encodestring('%s:%s' % (API_ID, API_SECRET)).replace('\n', '')
 
-# Set startDate of timeseries
+# Set startDate and endDate of timeseries
 # Could be an empty string to start with first measurement, or any datetime string according to the ISO-8601 standard
-startDate 				= ''
+startDate 				= '2016-06-30'
+endDate 				= '2016-07-11'
 
 # Function to request data from API
 def apiRequest (url, params = {}):
@@ -75,12 +76,12 @@ elif(decodedResponse['status'] == 'success'):
 
 		# The timeseries call currently only returns 4 weeks of data, therefore a while loop is used until the last data is retrieved (maximum attempts accounts for 2 years of data)
 		# This will be improved in future versions of the API
-		while (lastMeasurement < station['lastMeasurement'][0:13] and attempt < (2 * 4 * 13)):
+		while ((lastMeasurement < station['lastMeasurement'][0:13] and lastMeasurement < endDate) and attempt < (2 * 4 * 13)):
 
 			attempt 				+= 1;
 
 			# Request timeseries for specific station
-			tsResponse 				= apiRequest("https://tahmoapi.mybluemix.net/v1/timeseries/" + station['id'] + "/hourly", { 'startDate': lastMeasurement })
+			tsResponse 				= apiRequest("https://tahmoapi.mybluemix.net/v1/timeseries/" + station['id'] + "/", { 'startDate': lastMeasurement, 'endDate': endDate })
 			decodedTsResponse		= json.loads(tsResponse)
 
 			# Check if API responded with an error
@@ -109,19 +110,22 @@ elif(decodedResponse['status'] == 'success'):
 
 						# Create csv writer object
 						writer 			= csv.writer(csvFile)
-
 						line 			= 0;
 
-						# Loop through variable specific timeseries
-						for timestamp, value in sorted(decodedTsResponse['timeseries'][variable].items()):
+						try:
+							# Loop through variable specific timeseries
+							for timestamp, value in sorted(decodedTsResponse['timeseries'][variable].items()):
 
-							line 					+= 1 
+								line 					+= 1 
 
-							# Skip first measurement if it isn't the first request to prevent duplicates
-							if line > 1 or attempt == 1:
-								# Write value to csv file
-								writer.writerow([timestamp] + [value])
+								# Skip first measurement if it isn't the first request to prevent duplicates
+								if line > 1 or attempt == 1:
+									# Write value to csv file
+									writer.writerow([timestamp] + [value])
 
-							# Update lastMeasurement variable of csv file
-							if timestamp > lastMeasurement:
-								lastMeasurement 	= timestamp
+								# Update lastMeasurement variable of csv file
+								if timestamp > lastMeasurement:
+									lastMeasurement 	= timestamp
+
+						except:
+							print "Could not process variable ", variable
